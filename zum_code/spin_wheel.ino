44942d887fe7d39435d13a9eaca5e9a75796be41
temameta <62208316+temameta@users.noreply.github.com>
2023-01-14 23:26:31 +0300

    Update spin_wheel.ino

--- zum_code/spin_wheel.ino
+++ zum_code/spin_wheel.ino
@@ -1,20 +1,20 @@
 #include <Servo.h>
 
-const byte MOTOR_PORT = 9;
+#define MOTOR_PORT 9
+#define BQ_BLUETOOTH 19200
+#define PISTON_DELAY 4
 
-bool motor_status = false;
+Servo serv;
 
 void to_start();
 void to_container_N1();
 void to_container_N2();
 void checkData(char data);
 
-Servo serv;
-
 void setup() {
   pinMode(MOTOR_PORT, OUTPUT);
   serv.attach(MOTOR_PORT);
-  Serial.begin(19200);
+  Serial.begin(BQ_BLUETOOTH);
   
   to_start();
 }
@@ -26,7 +26,6 @@ void loop()
     checkData(dataBuffer);
   }
 }
-
 
 void checkData(char data)
 {  
@@ -58,7 +57,7 @@ void to_container_N1()
 {
   serv.write(180);
   delay(500);
-  //delay(10000);
+  //delay(PISTON_DELAY*1000);
   to_start();
 }
 
@@ -66,9 +65,6 @@ void to_container_N2()
 {
    serv.write(0);
    delay(500);
-   //delay(10000);
+   //delay(PISTON_DELAY*1000);
    to_start();
 }
-
-
-
