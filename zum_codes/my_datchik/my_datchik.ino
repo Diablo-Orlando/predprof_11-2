8a222700eff31b41f8e3fecc35361a907e7153eb
temameta <sezemov2@gmail.com>
2023-01-19 23:12:45 +0300

    Update my_datchik.ino

--- zum_codes/my_datchik/my_datchik.ino
+++ zum_codes/my_datchik/my_datchik.ino
@@ -1,20 +1,24 @@
-#define s0 0
-#define s1 1
+#define s0 4
+#define s1 3
 #define s2 2
-#define s3 3
-#define sOut 4
+#define s3 1
+#define sOut 0
 
 int redfrequency = 0;
 int bluefrequency = 0;
 int greenfrequency = 0;
 
+int rm = 0;
+int gm = 0;
+int bm = 0;
+
+int r = 1000;
+int g = 1000;
+int b = 1000;
+
 int redColor = 0;
 int greenColor = 0;
 int blueColor = 0;
-
-int r = 0;
-int g = 0;
-int b = 0;
 
 void checkData(char data);
 void scan();
@@ -38,47 +42,94 @@ void loop() {
 //  if (Serial.available() > 0) { 
 //    char dataBuffer = Serial.read();
 //    checkData(dataBuffer);
+//    }
 }
 
-void checkData(char data)
-{  
+void checkData(char data) {  
     switch(data){
-      case '1': 
+      case 'S':
       scan();
-      break;
-      
-      default: 
-      Serial.println("ERROR");
+//      default: 
+//          Serial.println("ERROR");
+//          break;
         }
-  }
+}
 
 void scan()
 {
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
   redfrequency = pulseIn(sOut, LOW);
-  redColor = map(redfrequency, 2, 283, 255, 0);
-  delay(100);
+  redColor = map(redfrequency, 162, 148, 255, 0);
+
+//  Serial.print("R = ");
+//  Serial.println(redfrequency);
+//  if (redfrequency > rm)
+//    rm = redfrequency;
+//  if (redfrequency < r)
+//    r = redfrequency;
+//  Serial.print("rmax = ");
+//  Serial.println(rm);
+//  Serial.print("rmin = ");
+//  Serial.println(r);
   
-  digitalWrite(s2, LOW);
-  digitalWrite(s3, HIGH);
-  bluefrequency = pulseIn(sOut, LOW);
-  blueColor = map(bluefrequency, 6, 65, 255, 0);
   delay(100);
   
   digitalWrite(s2, HIGH);
   digitalWrite(s3, HIGH);
   greenfrequency = pulseIn(sOut, LOW);
-  greenColor = map(greenfrequency, 12, 78, 255, 0);
+  greenColor = map(greenfrequency, 182, 206, 255, 0);
+
+
+//  Serial.print("B = ");
+//  Serial.println(bluefrequency);
+//  if (bluefrequency > bm)
+//    bm = bluefrequency;
+//  if (bluefrequency < b)
+//    b = bluefrequency;
+//  Serial.print("bmax = ");
+//  Serial.println(bm);
+//  Serial.print("bmin = ");
+//  Serial.println(b);
+
+  delay(100);
+
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, HIGH);
+  bluefrequency = pulseIn(sOut, LOW);
+  blueColor = map(bluefrequency, 126, 137, 255, 0);
+  
+  
+//  Serial.print("G = ");
+//  Serial.println(greenfrequency);
+//  if (greenfrequency > gm)
+//    gm = greenfrequency;
+//  if (greenfrequency < g)
+//    g = greenfrequency;
+//  Serial.print("gmax = ");
+//  Serial.println(gm);
+//  Serial.print("gmin = ");
+//  Serial.println(g);
+  
   delay(100);
   
   if(redColor > greenColor && redColor > blueColor){
      Serial.println("RED");
   }
-  if(greenColor > redColor && greenColor > blueColor){
+  else if(greenColor > redColor && greenColor > blueColor){
     Serial.println("GREEN");
   }
-  if(blueColor > redColor && blueColor > greenColor){
+  else if(blueColor > redColor && blueColor > greenColor){
     Serial.println("BLUE");
   }
+
+//  if(redfrequency > 148 && redfrequency < 162 && greenfrequency > 212 && greenfrequency < 239 && bluefrequency > 141 && bluefrequency < 163){
+//     Serial.println("RED");
+//  }
+//  else if(redfrequency > 153 && redfrequency < 169 && greenfrequency > 182 && greenfrequency < 206 && bluefrequency > 150 && bluefrequency < 170){
+//    Serial.println("GREEN");
+//  }
+//  else if(redfrequency > 126 && redfrequency < 139 && greenfrequency > 211 && greenfrequency < 226 && bluefrequency > 126 && bluefrequency < 137){
+//    Serial.println("BLUE");
+//  }
 }
